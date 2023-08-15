b78d82e15cfd5c7ffdbb22f59fb1172e4a45abd6
STEPHEN IGNATIUS <stephen.ignatius@gmail.com>
2023-08-15 13:53:34 +0100

    azure pipeline added..

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/home', (req, res) => {
-    console.log('Home page!')
+    console.log('Home page azure pipeline!')
     res.status(200).send('Home page!');
 })
 
 app.get('/about', (req, res) => {
-    console.log('About page!')
+    console.log('About page azure pipeline!')
     res.status(200).send('About page!');
 })
 
 app.get('/contact', (req, res) => {
-    console.log('Contact page!')
+    console.log('Contact page azure pipeline!')
     res.status(200).send('Contact page!');
 })
 
